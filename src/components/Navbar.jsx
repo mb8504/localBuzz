06dce9095562257d82a7d77afaf65ba76bd523be
mb8504 <mbergmarketing@gmail.com>
2024-05-20 08:48:05 -0400

    finished navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,20 +13,20 @@ const Navbar = () => {
     return (
       <section className='bg-black'>
         <nav className="font-Roboto flex justify-between siteContainer">
-            <a href="#" className='font-Logo text-white'>Local Buzz Brewing</a>
+            <a href="#" className='font-Logo text-white text-3xl pt-5 pb-5 pr-5'>Local Buzz Brewing</a>
 
             <button onClick={handleNav} className='z-50 text-white md:max-xl:hidden lg:hidden xl:hidden 2xl:hidden'>
                 {!nav ? <AiOutlineMenu size={40} /> : <AiOutlineClose size={40} />}
             </button>
 
-            <div id='mobile' className={!nav ? 'fixed right-[-100%]' : 'fixed right-0 top-0 w-[60%] h-full bg-slate-900'}>
+            <div id='mobile' className={!nav ? 'fixed right-[-100%]' : 'fixed right-0 top-0 w-[100%] h-full bg-black bg-opacity-90'}>
 
-            <ul className='flex flex-col text-white'>
-                <li><a href="#">Home</a></li>
-                <li className='ml-8'><a href="#">Beers</a></li>
-                <li className='ml-8'><a href="#">Events</a></li>
-                <li className='ml-8'><a href="#">Rhythm & Blues</a></li>
-                <li className='ml-8'><a href="#">Contact</a></li>
+            <ul className='flex flex-col text-white text-base text-center font-bold'>
+                <li className='mt-20 pt-8'><a href="#">Home</a></li>
+                <li className='mt-8'><a href="#">Beers</a></li>
+                <li className='mt-8'><a href="#">Events</a></li>
+                <li className='mt-8'><a href="#">Rhythm & Blues</a></li>
+                <li className='mt-8'><a href="#">Contact</a></li>
             </ul>
 
             </div>
