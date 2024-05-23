f3cb1e493fcaebc79eafb2eaec39d5dfbcab6de0
mb8504 <mbergmarketing@gmail.com>
2024-05-23 17:33:00 -0400

    carousel functionality working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,27 +4,27 @@ function Header() {
   return (
     <>
       <img className='w-full' src="src/assets/hero.png" alt="" />
-      <div className='bg-black pt-14 pb-14'>
+      <div className='bg-black pt-8 pb-8'>
         <section className="siteContainer">
-          <div className="text-white flex justify-between text-center">
-              <div className="result">
-                  <div className="number text-4xl font-bold">5</div>
+          <div className="text-white grid grid-cols-3 md:flex justify-between text-center">
+              <div className="result p-4">
+                  <div className="number text-4xl font-bold">7</div>
                   <div className="text">Years</div>
               </div>
-              <div className="result">
+              <div className="result p-4">
                   <div className="number text-4xl font-bold">14</div>
                   <div className="text">Staff</div>
               </div>
-              <div className="result">
-                  <div className="number text-4xl font-bold">1,503,252</div>
+              <div className="result p-4 ">
+                  <div className="number text-4xl font-bold">1M</div>
                   <div className="text">Gallons of Beer</div>
               </div>
-              <div className="result">
-                  <div className="number text-4xl font-bold">109,500</div>
-                  <div className="text">Quenched</div>
+              <div className="result p-4 ">
+                  <div className="number text-4xl font-bold">9</div>
+                  <div className="text">Awards</div>
               </div>
-              <div className="result">
-                  <div className="number text-4xl font-bold">200,000</div>
+              <div className="result p-4">
+                  <div className="number text-4xl font-bold">55K</div>
                   <div className="text">Charity</div>
               </div>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ export default {
       fontFamily: {
         Roboto: ['Roboto', 'sans-serif'],
         Logo: ["Courgette", 'cursive'],
+      },
+      width: {
+        '1/5': '20%', // Custom width
+        '24.6': '24.6%',  // Slightly less than 25%
+      },
+      colors: {
+        'orange': '#F0840A'
       }
     },
   },
