e9cb5a0f88b4937efe0a9ef298db49eca1774965
mb8504 <mbergmarketing@gmail.com>
2024-06-06 08:21:48 -0400

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://mb8504.github.io/",
   plugins: [react()],
   server: {
     port: 3000,
